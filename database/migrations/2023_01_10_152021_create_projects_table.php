e3a6ec05a8111974af6bc6b7960eee40147827aa
SimoneLollini <lollinisimone@gmail.com>
2023-01-10 17:08:38 +0100

    Add authorization

--- database/migrations/2023_01_10_152021_create_projects_table.php
+++ database/migrations/2023_01_10_152021_create_projects_table.php
@@ -16,6 +16,8 @@ return new class extends Migration
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->string('description')->nullable();
+            $table->string('slug')->nullable();
             $table->timestamps();
         });
     }
