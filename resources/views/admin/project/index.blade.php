4f0aab8078967779793b87e35ebee76ab331dc80
SimoneLollini <lollinisimone@gmail.com>
2023-01-11 16:39:37 +0100

    refactoring index and edit

--- resources/views/admin/project/index.blade.php
+++ resources/views/admin/project/index.blade.php
@@ -4,8 +4,21 @@
 @section('title')
 Tools
 @endsection
+@if(session('message'))
+<div class="alert alert-success alert-dismissible fade show" role="alert">
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    <strong>
+        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
+            <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
+        </svg>
+        <span class="info_message">
+            {{session('message')}}
+        </span>
+    </strong>
+</div>
+@endif
 <div class="container">
-    <a class="btn btn-warning m-3" href="{{route('project.create')}}">Add</a>
+    <a class="btn btn-primary m-3" href="{{route('project.create')}}">Add</a>
     <div class="table-responsive">
         <table class="table table-primary">
             <thead>
@@ -23,12 +36,12 @@ Tools
                     <td> {{$project->description}}</td>
                     <td> {{$project->slug}}</td>
                     <td class="d-flex">
-                        <a class="btn btn-success m-3 w-75" href="{{route('project.show', $project->id)}}">Details</a>
-                        <a class="btn btn-warning m-3 w-75" href="{{route('project.edit', $project->id)}}">Edit</a>
+                        <a class="btn btn-primary m-3 w-75" href="{{route('project.show', $project->id)}}">Details</a>
+                        <a class="btn btn-primary m-3 w-75" href="{{route('project.edit', $project->id)}}">Edit</a>
                         <form action="{{route('project.destroy', $project->id)}}" method="POST">
                             @csrf
                             @method('DELETE')
-                            <input class="btn btn-danger m-3 w-75" type="submit" value="Delete">
+                            <input class="btn btn-primary m-3 w-75" type="submit" value="Delete">
                         </form>
                     </td>
                 </tr>
